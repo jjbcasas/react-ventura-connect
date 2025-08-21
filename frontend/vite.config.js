868e676f6ca95ef2662e7890e59a9dde79a97db8
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-08-21 01:03:16 -0700

    made changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://react-ventura-connect-backend.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
