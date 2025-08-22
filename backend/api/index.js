a3d566c77877c6b08b4a76166ee89830708dd614
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-08-22 12:25:38 -0700

    made changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -50,12 +50,11 @@ app.use(express.json())
 app.use(logger('dev'))
 
 // cors middleware
-app.use(cors(
-    // {
-    //     origin: `${process.env.FRONTEND_URL}`,
-    //     credentials: true // If you're sending cookies/sessions
-    // }
-    )
+app.use(
+    cors({
+        origin: `${process.env.FRONTEND_URL}`,
+        credentials: true // If you're sending cookies/sessions
+    })
 )
 
 // setup sessions - stored in mongodb
