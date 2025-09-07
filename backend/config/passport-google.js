a72f984a3fb6e3b35cb203282f94960038f827f7
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-09-07 15:16:07 -0700

    made changes

--- backend/config/passport-google.js
+++ backend/config/passport-google.js
@@ -10,7 +10,7 @@ export default function(passport){
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `https://ventura-connect.onrender.com/auth/google/callback`,
+        callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
     },
     async ( accessToken, refreshToken, profile, done) => {
         try {
