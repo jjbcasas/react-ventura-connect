acc02f2401c6a2e606abbfb66d6fb88c92164555
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-09-07 13:26:29 -0700

    made changes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,6 @@ export const ensureAuth = ( req, res, next ) => {
     if ( req.isAuthenticated()) {
         return next()
     } else {
-        console.log(req.user)
         res.status(401).json({ message: 'Unauthorized' })
     }
 }
