8a66d1a913fb4c789b840da77a26df465cb63d6c
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-09-07 13:32:02 -0700

    made changes

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -16,6 +16,6 @@ router.post('/signup', postSignup)
 
 // Google Routes
 router.get('/auth/google', passport.authenticate('google', { scope: ['email','profile'], prompt: 'select_account'}))
-router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: `${process.env.FRONTEND_URL}/login` }), googleCallback)
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: `${process.env.FRONTEND_URL}/login`, successRedirect: `${process.env.FRONTEND_URL}/feed` }), googleCallback)
 
 export default router
