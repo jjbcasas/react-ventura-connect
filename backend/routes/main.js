06712872b3d35e00d39a944f7cf3cb2a595811ad
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-09-07 00:19:44 -0700

    made changes

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -10,7 +10,7 @@ dotenv.config({ path: './backend/config/.env'})
 // Auth Routes
 router.get('/user', ensureAuth, getUser)
 // router.get('/login', getUser)
-router.post('/login', postLogin)
+router.post('/login', ensureAuth, postLogin)
 router.post('/logout', logout)
 // router.get('/signup', getSignup)
 router.post('/signup', postSignup)
