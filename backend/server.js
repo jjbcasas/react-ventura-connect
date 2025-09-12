1834a53febb4589b0dd9d581e151f6f7e54e1c0d
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-09-12 14:59:27 -0700

    made changes

--- backend/server.js
+++ backend/server.js
@@ -50,11 +50,11 @@ connectDB()
 // static folder
 app.use(express.static('public'))
 
-// app.use(express.static(path.join(__dirname, 'dist')))
-app.use(express.static(path.join(__dirname, '../frontend/dist')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'))
-})
+app.use(express.static(path.join(__dirname, 'dist')))
+// app.use(express.static(path.join(__dirname, '../frontend/dist')))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'))
+// })
 
 // body parsing, so we can pull something from the request
 app.use(express.urlencoded({extended: true}))
