36d3a5544e4710b86d4cf964053de9618d4daed1
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-09-23 22:31:36 -0700

    made changes

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -1,17 +1,17 @@
 import express from 'express'
 const router = express.Router()
-import { getFeed, createPost, likePost, minusLike, deletePost, createComment, followUser, unfollowUser } from '../controllers/feed.js'
+import { getFeed, createPostInFeed, likePostInFeed, minusLikeInFeed, /*deletePostInFeed,*/ createCommentInFeed, followUserInFeed, unfollowUserInFeed } from '../controllers/feed.js'
 import upload  from '../middleware/multer.js'
 import { ensureAuth } from '../middleware/auth.js'
 
 // Feed Routes
 router.get('/', ensureAuth, getFeed)
-router.post('/createPost', upload.single('file'), createPost)
-router.put('/likePost/:id', likePost)
-router.put('/minusLike/:id', minusLike)
-router.delete('/deletePost/:id', deletePost)
-router.post('/comments/:id', createComment)
-router.put('/followUser/:id', followUser)
-router.put('/unfollowUser/:id', unfollowUser)
+router.post('/createPost', upload.single('file'), createPostInFeed)
+router.put('/likePost/:id', likePostInFeed)
+router.put('/minusLike/:id', minusLikeInFeed)
+// router.delete('/deletePost/:id', deletePostInFeed)
+router.post('/comments/:id', createCommentInFeed)
+router.put('/followUser/:id', followUserInFeed)
+router.put('/unfollowUser/:id', unfollowUserInFeed)
 
 export default router
--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -2,18 +2,18 @@ import express from 'express'
 const router = express.Router()
 import upload from '../middleware/multer.js'
 import { ensureAuth } from '../middleware/auth.js'
-import { getProfile, createPost, likePost, minusLikePost, deletePost, followUser, unfollowUser, uploadProfilePhoto, changeProfilePhoto, createComment } from '../controllers/profile.js'
+import { getProfile, createPostInProfile, likePostInProfile, minusLikeInProfile, deletePostInProfile, followUserInProfile, unfollowUserInProfile, uploadProfilePhotoInProfile, changeProfilePhotoInProfile, createCommentInProfile } from '../controllers/profile.js'
 
 // Profile Routes
 router.get('/:id', ensureAuth, getProfile)
-router.post('/createPost', upload.single('file'), createPost)
-router.put('/likePost/:id', likePost)
-router.put('/minusLikePost/:id', minusLikePost)
-router.delete('/deletePost/:id', deletePost)
-router.put('/followUser/:id', followUser)
-router.put('/unfollowUser/:id', unfollowUser)
-router.put('/uploadProfilePhoto', upload.single('file'), uploadProfilePhoto)
-router.put('/changeProfilePhoto', upload.single('file'), changeProfilePhoto)
-router.post('/comments/:id', createComment)
+router.post('/createPost', upload.single('file'), createPostInProfile)
+router.put('/likePost/:id', likePostInProfile)
+router.put('/minusLikePost/:id', minusLikeInProfile)
+router.delete('/deletePost/:id', deletePostInProfile)
+router.put('/followUser/:id', followUserInProfile)
+router.put('/unfollowUser/:id', unfollowUserInProfile)
+router.put('/uploadProfilePhoto', upload.single('file'), uploadProfilePhotoInProfile)
+router.put('/changeProfilePhoto', upload.single('file'), changeProfilePhotoInProfile)
+router.post('/comments/:id', createCommentInProfile)
 
 export default router
